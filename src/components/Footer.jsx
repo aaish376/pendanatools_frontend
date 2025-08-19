e6807642467f4c9978bfc045d0fdba46d08c32f3
aaish376 <bcsf21m.pucit@gmail.com>
2025-08-20 02:30:12 +0500

    add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ function Footer() {
     <footer className="bg-gray-800 text-white p-6 text-center">
       <p>© 2025 PendanaTools. All rights reserved.</p>
       <div className="mt-2 space-x-4">
-        <a href="#" className="hover:underline">Facebook</a>
-        <a href="#" className="hover:underline">Twitter</a>
+        <a href="https://asadalihere.netlify.app" className="hover:underline">Facebook</a>
+        <a href="https://asadalihere.netlify.app" className="hover:underline">Twitter</a>
         <a href="mailto:contact@pendanatools.site" className="hover:underline">Email</a>
       </div>
     </footer>
